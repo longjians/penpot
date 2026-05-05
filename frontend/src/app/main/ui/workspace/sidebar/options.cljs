faa9ce8fdd6cf83936522be9af6a1c16963e661c
Elena Torro <elenatorro@gmail.com>
2026-05-05 16:28:03 +0200

    :zap: Increase throttling on shape options

--- frontend/src/app/main/ui/workspace/sidebar/options.cljs
+++ frontend/src/app/main/ui/workspace/sidebar/options.cljs
@@ -73,7 +73,8 @@
       nil)))
 
 (mf/defc shape-options*
-  {::mf/private true}
+  {::mf/wrap [#(mf/throttle % 200)]
+   ::mf/private true}
   [{:keys [shapes shapes-with-children selected page-id file-id libraries]}]
   (if (= 1 (count selected))
     [:> single-shape-options*
